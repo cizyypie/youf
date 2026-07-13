0362cfd4a492cfaf060a6714f059b58a85bfe88a
Joan <joanitatimbinp@gmail.com>
2026-07-13 20:47:49 +0700

    feat: add cron for scheduling order cancle not paid order

--- services/order-service/src/db/client.ts
+++ services/order-service/src/db/client.ts
@@ -1,5 +1,9 @@
 import {drizzle} from 'drizzle-orm/postgres-js';
 import postgres from 'postgres'; 
 
-const queryClient = postgres (process.env.DATABASE_URL!)
-export const db = drizzle(queryClient)
+
+const connectionString = process.env.DATABASE_URL!;
+export const db = drizzle(postgres(connectionString, { 
+  max: 1, 
+  connect_timeout: 30
+}));
